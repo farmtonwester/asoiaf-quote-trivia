a1bec6a626ef75c73629d34e579386f5b422edf3
Weston Farmer <wesfarmer11@gmail.com>
2024-07-20 12:24:52 -0500

    initialize db.json with sample quotes, add getQuote fetch request

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-quoteCard = document.getElementById("quote-card");
+// Maximum number to be generated, equal to number of quoteObjects available in db.json
+const maxQuotes = 4; 
+const quoteCard = document.getElementById("quote-card");
+
+
+// Use Math object to get random int
+// fetch quoteObj[randomNumber]
+function getQuote() {
+    const randomNumber = Math.floor(Math.random() * maxQuotes)
+    const newQuote = fetch("http://localhost:3000/quotes")
+    .then(resp => resp.json()
+    .then(data => {
+        const quoteObj = data[randomNumber];
+        const text = quoteCard.createElement("p");
+        text.textContent = quoteObj.quote
+    }))
+}
+
+document.addEventListener("DOMContentLoaded", 
+    getQuote()
+);
